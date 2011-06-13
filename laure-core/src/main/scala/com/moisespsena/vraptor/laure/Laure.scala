89ab644bb053b74e4b2b66d72a9e7f8490b472f7
Moisés P. Sena <moisespsena@gmail.com>
2011-06-13 13:22:58 -0300

    First Commit

---
+++ laure-core/src/main/scala/com/moisespsena/vraptor/laure/Laure.scala
@@ -0,0 +1,11 @@
+package com.moisespsena.vraptor.laure
+
+/** The Vraptor Laure Info
+ *
+ *  @author Moisés P. Sena &lt;moisespsena@gmail.com&gt;
+ */
+object Laure {
+	def main(args: Array[ String ]): Unit = {
+		println("Vraptor Lauré Plugin");
+	}
+}
